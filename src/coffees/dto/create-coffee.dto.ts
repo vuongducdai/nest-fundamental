7928d470140a5b3a953248f4328099085d145a02
daivd <daivuong7696@gmail.com>
2025-09-17 12:52:46 +0700

    Validate and Transform DTO

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from "class-validator";
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsString()
   readonly flavors: string[];
 }
